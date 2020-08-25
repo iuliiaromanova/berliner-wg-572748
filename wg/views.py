078af32347793e18c4ae16782f9423faab7bd000
iuliia romanova <romanovajb@gmail.com>
2020-08-25 12:05:26 +0200

    Modified templates to display posts from database.

--- wg/views.py
+++ wg/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Anzeige
 
 # Create your views here.
 def anzeige_list(request):
-    return render(request, 'wg/anzeige_list.html', {})
+    anzeiges = Anzeige.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'wg/anzeige_list.html', {'anzeiges': anzeiges})
