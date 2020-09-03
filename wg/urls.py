cf4b45cd5a1fa74f54f76d60c78a21eff6d5ab3d
iuliia romanova <romanovajb@gmail.com>
2020-09-03 09:55:35 +0200

    Added view and template for detailed wg anzeige as well as CSS for the site

--- wg/urls.py
+++ wg/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.anzeige_list, name='anzeige_list'),
+    path('anzeige/<int:pk>/', views.anzeige_detail, name='anzeige_detail'),
 ]
--- wg/views.py
+++ wg/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Anzeige
 
@@ -6,3 +6,8 @@ from .models import Anzeige
 def anzeige_list(request):
     anzeiges = Anzeige.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'wg/anzeige_list.html', {'anzeiges': anzeiges})
+
+#Anzeige.objects.get(pk=pk)
+def anzeige_detail(request, pk):
+    anzeige = get_object_or_404(Anzeige, pk=pk)
+    return render(request, 'wg/anzeige_detail.html', {'anzeige': anzeige})
